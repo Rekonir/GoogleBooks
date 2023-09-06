e091fc4e81f5f0d1a26c50367e4d1c86fcbf5989
Rekonir <zhed-nikita@mail.ru>
2023-09-06 16:55:26 +0300

    add filter

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -9,14 +9,18 @@ const MainPage = () => {
     const [search, setSearch] = useState("")
     const [booksData, setBooksData] = useState<Array<IBook>>([])
     const [curentBook, setCurentBook] = useState(0)
+    const [category, setCategory] = useState('all')
+    const [sort, setSort] = useState('relevance')
+
+
 
     useEffect(() => {
         setCurentBook(booksData.length)
     }, [booksData])
-    
-    
+
+
     const nextBook = () => {
-        const url = `https://www.googleapis.com/books/v1/volumes?q=${search}&key=${API_KEY}&maxResults=${cardPerPage}&startIndex=${curentBook}`;
+        const url = `https://www.googleapis.com/books/v1/volumes?q=${search}&key=${API_KEY}&maxResults=${cardPerPage}&startIndex=${curentBook}&orderBy${sort}`;
         axios
             .get(url)
             .then(res => setBooksData(booksData.concat(res.data.items)))
@@ -27,11 +31,10 @@ const MainPage = () => {
 
     const fetchData = () => {
         setBooksData([])
-        const url = `https://www.googleapis.com/books/v1/volumes?q=${search}&key=${API_KEY}&maxResults=${cardPerPage}&startIndex=0`
+        const url = `https://www.googleapis.com/books/v1/volumes?q=${search}&key=${API_KEY}&maxResults=${cardPerPage}&startIndex=0&orderBy${sort}`
         axios.get(url)
             .then(res => setBooksData(res.data.items))
             // .then(res => console.log(booksData.concat(res.data)))
-
             .catch(err => console.log(err))
 
     }
@@ -58,13 +61,33 @@ const MainPage = () => {
                         <img src="../../src/assets/imgs/search-svgrepo-com.svg" alt="" className={styles.searchIcon} />
                     </button>
                 </div>
+                <select name="category" id="category" defaultValue={category} onChange={e => setCategory(e.target.value)}>
+                    <option value="all"> Всё </option>
+                    <option value="Art"> Арт</option>
+                    <option value="Biography">Биография </option>
+                    <option value="Computers">Компьютеры</option>
+                    <option value="History"> История </option>
+                    <option value="Medical"> Медицина </option>
+                    <option value="Poetry"> Поэзия</option>
+                </select>
+                <select name="sort" id="sort" defaultValue={sort} onChange={e => setSort(e.target.value)}>
+                    <option value="relevance">по релевантности </option>
+                    <option value="newest">по новизне</option>
+                </select>
             </div>
             <div className={styles.catalog}>
-                {booksData.map((book: IBook) => {
-                    return (
-                        <Card book={book} key={book.id} />
-                    )
-                })
+                {category !== 'all' ?
+                    booksData.filter((book) => book?.volumeInfo?.categories?.includes(category)).map((book: IBook) => {
+                        return (
+                            <Card book={book} key={book.id} />
+                        )
+                    })
+                    :
+                    booksData.map((book: IBook) => {
+                        return (
+                            <Card book={book} key={book.id} />
+                        )
+                    })
                 }
             </div>
             <button className={styles.more} onClick={nextBook}>
